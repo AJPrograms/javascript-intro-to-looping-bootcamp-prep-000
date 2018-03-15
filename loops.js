db5c05756816f1c84078a79bd18db6a693d648b7
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-15 04:51:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,17 +1,20 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++)
-  if (i === 1) {
+  for (let i = 0; i < 25; i++) {
+      if (i === 1) {
     array.push(`I am ${i} strange loop.`);
   }  
   else {
     array.push(`I am ${i} strange loops.`);
   }  
   return array;
+  }
 }
+
 
 function whileLoop(n) {
   while (n > 0) {
-    console.log(--n)
+    console.log(--n);
   }
-  return "done"
+  return "done";
 }
+
