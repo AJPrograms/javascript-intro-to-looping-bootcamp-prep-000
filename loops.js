2fc9871caa27ef63926bf4b9cf0c56b5a95f79a0
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-15 04:44:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < 25; i++)
     if (i === 1) {
       array.push(`I am ${i} strange loop.`);
     }  
@@ -8,5 +8,4 @@ function forLoop(array) {
     }  
     return array;
   }
-}
 
